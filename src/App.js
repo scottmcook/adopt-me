78d0d19ca63a70182104381188bf8fec33bc33d6
scottmcook <scott.madison.cook@gmail.com>
2023-10-17 11:01:17 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+function Pet(props) {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+}
+
+// App and components are always capitalized.
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Laura",
+      animal: "Dog",
+      breed: "Australian Cattle Dog",
+    }),
+    React.createElement(Pet, {
+      name: "Moody",
+      animal: "Dog",
+      breed: "Australian Cattle Dog",
+    }),
+    React.createElement(Pet, {
+      name: "Binx",
+      animal: "Cat",
+      breed: "hocus",
+    })
+  );
+};
+const container = document.getElementById("root");
+// New way of rendering instead of ReactDOM.render.
+// It's also a way to opt into new features.
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
