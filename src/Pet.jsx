1f43727e24139d17853e86ed5ce6b5da52756f3a
Scott Cook <scott.madison.cook@gmail.com>
2023-11-27 16:07:13 -0800

    Update props

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
-const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
 
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
+
   return (
     <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
-        <div className="info">
-          <h1>{name}</h1>
-          <h2>
-            {animal} - {breed} - {location}
-          </h2>
-        </div>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
     </Link>
   );
