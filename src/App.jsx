3e26849b1596ec9f9aa6e25baebb2800369127bd
Scott Cook <scott.madison.cook@gmail.com>
2023-11-28 09:57:16 -0800

    Add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 // App and components are always capitalized.
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Adopt Me!</h1>
-        <Routes>
-          <Route path="/details/:id" element={<Details />}></Route>
-          <Route path="/" element={<SearchParams />}></Route>
-        </Routes>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <h1>Adopt Me!</h1>
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams />}></Route>
+          </Routes>
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
