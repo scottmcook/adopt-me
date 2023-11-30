9685e6013b84bda2f46ad85745fa2fbd943688b2
Scott Cook <scott.madison.cook@gmail.com>
2023-11-29 20:21:21 -0600

    Update to use https

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -2,14 +2,14 @@ async function fetchSearch({ queryKey }) {
   const { animal, location, breed } = queryKey[1];
 
   const res = await fetch(
-    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
   );
 
   if (!res.ok) {
     throw new Error(`pet search not oke ${animal}, ${location}, ${breed}`);
   }
 
-  console.log(res.json());
+  // console.log(res.json());
   return res.json();
 }
 
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -4,5 +4,6 @@ import fetchBreedList from "./fetchBreedList";
 export default function useBreedList(animal) {
   const results = useQuery(["breeds", animal], fetchBreedList);
 
+  // Return an empty array if I don't have
   return [results?.data?.breeds ?? [], results.status];
 }
